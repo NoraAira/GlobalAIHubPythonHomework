373005c5f98089914ba1b1dfe570821121016cac
NoraAira <76444064+NoraAira@users.noreply.github.com>
2020-12-22 22:53:48 +0300

    Add files via upload

---
+++ odev0.py
@@ -0,0 +1,24 @@
+e = 8
+m = 8.3
+i = "Emir"                       #değişkenlere değer atadım
+r = 7
+a = "E"
+####################################################
+####################################################
+print(f'Values: {e}')
+print(f'Values: {m}')
+print("Values: {}".format(i))       
+print("Values: {}".format(r))
+print("Values: {}".format(a))
+"""
+ YAZI TİPLERİ   
+#float=1.0
+#int=1
+#str='emir'
+#bloom= True , False
+"""
+print(f'Types: {type(e)}')               #float=1.0
+print(f'Types: {type(m)}')               #int=1
+print("Types: {}".format(type(i)))       #str= 'emir'       
+print("Types: {}".format(type(r)))       #bloom= True , False
+print("Types: {}".format(type(a)))
